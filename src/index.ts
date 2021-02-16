c16ea516c49449095fd1b4e0b674a2cf3472e943
GO Lee <52603436+goleedev@users.noreply.github.com>
2021-02-16 19:03:21 +0900

    0.1 Overloads fixed

--- src/index.ts
+++ src/index.ts
@@ -82,6 +82,11 @@ function sumEverything(...nums: number[]):number {
     return nums.reduce((result, num) => result + num, 0 )
 }
 
-function calcArea(width: number, height: number):number {
-    
-}
+function calcArea(width: number, height: number):number;
+function calcArea(length: number):number;
+function calcArea(...args:number[]):number {
+    if(args.length === 2) {
+        return args[0] * args[1];
+    }
+    return Math.pow(args[0], 2)
+};
