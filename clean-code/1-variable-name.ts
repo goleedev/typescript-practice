d446ef88286a69bffa983866944b583f87aeaec1
GO Lee <golee.dev@gmail.com>
2021-02-20 23:05:31 +0900

    clean-code practice 2

--- clean-code/1-variable-name.ts
+++ clean-code/1-variable-name.ts
@@ -1,3 +1,4 @@
+// Use meaningful varibale names
 {
   // Bad
   function betweenBad<T>(a1: T, a2: T, a3: T): boolean {
@@ -8,4 +9,43 @@
   function betweenGood<T>(value: T, left: T, right: T): boolean {
     return left <= value && value <= right;
   }
+}
+
+// Use pronouceable varible names
+{
+  // Bad
+  type DtaRcrd102 = {
+    genymdhms: Date;
+    modymdhms: Date;
+    pszqint: number;
+  };
+
+  // Good
+  type Customer = {
+    generationTimestamp: Date;
+    modificationTimestamp: Date;
+    recordId: number;
+  };
+}
+
+// Use same vocab for the same type of variable
+{
+  // Bad
+  function getUserInfo(): User;
+  function getUserDetails(): User;
+  function getUserData(): User;
+
+  // Good
+  function getUser(): User;
+}
+
+// Use searchable names
+{
+  // Bad
+  setTimeout(restart, 86400000);
+
+  // Good
+  const MILLISECONDS_IN_A_DAY = 24 * 60 * 60 * 1000;
+
+  setTimeout(restart, MILLISECONDS_IN_A_DAY);
 }
