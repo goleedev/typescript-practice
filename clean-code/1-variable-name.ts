c94cfb29032696b6cdcb7060b4f121bb8374ef08
GO Lee <golee.dev@gmail.com>
2021-02-21 20:15:00 +0900

    clean-code practice 3

--- clean-code/1-variable-name.ts
+++ clean-code/1-variable-name.ts
@@ -49,3 +49,58 @@
 
   setTimeout(restart, MILLISECONDS_IN_A_DAY);
 }
+
+// Use explanatory variables
+{
+  // Bad
+  declare const users: Map<string, User>;
+
+  for (const keyValue of users) {
+    // iterate through users map
+  }
+
+  // Good
+  declare const users: Map<string, User>;
+
+  for (const [id, user] of users) {
+    // iterate through users map
+  }
+}
+
+// Avoid Mental Mapping
+{
+  // Bad
+  const u = getUser();
+  const s = getSubscription();
+  const t = charge(u, s);
+
+  // Good
+  const user = getUser();
+  const subscription = getSubscription();
+  const transaction = charge(user, subscription);
+}
+
+// Only add needed context
+{
+  // Bad
+  type Car = {
+    carMake: string;
+    carModel: string;
+    carColor: string;
+  };
+
+  function print(car: Car): void {
+    console.log(`${car.carMake} ${car.carModel} (${car.carColor})`);
+  }
+
+  // Good
+  type Car = {
+    make: string;
+    model: string;
+    color: string;
+  };
+
+  function print(car: Car): void {
+    console.log(`${car.make} ${car.model} (${car.color})`);
+  }
+}
